f13e4b5c878a3f7c26eb41fa5165f69dc5cea40a
D@re <56046022+DigitalN8m4r3@users.noreply.github.com>
2022-10-19 00:32:47 +0200

    fixes to avatars
    removed shadow, couldnt get it to work properly

--- dojo_core/managers/HapticManager.gd
+++ dojo_core/managers/HapticManager.gd
@@ -28,19 +28,6 @@ func right_haptic_pulse_on_pickup(_what):
 	right_controller.set_rumble(0.2)
 	yield(get_tree().create_timer(0.2), "timeout")
 	right_controller.set_rumble(0.0)
-	
-func left_haptic_pulse_on_button(button_id):
-	if (button_id == left_controller.get_node("Function_Pickup").action_button_id) and left_controller.get_node("Function_Pickup").picked_up_object is Gun:
-		left_controller.set_rumble(0.5)
-		yield(get_tree().create_timer(0.2), "timeout")
-		left_controller.set_rumble(0.0)
-		
-func right_haptic_pulse_on_button(button_id):
-	if (button_id == right_controller.get_node("Function_Pickup").action_button_id) and right_controller.get_node("Function_Pickup").picked_up_object is Gun:
-		right_controller.set_rumble(0.5)
-		yield(get_tree().create_timer(0.2), "timeout")
-		right_controller.set_rumble(0.0)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
